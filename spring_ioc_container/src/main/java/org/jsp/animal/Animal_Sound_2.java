b4cce721b7c7d07a7073792a6842d7201d344252
ManishDevlikar <manish.devlikar5003@gmail.com>
2023-11-24 18:11:44 +0530

    added

--- spring_ioc_container/src/main/java/org/jsp/animal/Animal_Sound_2.java
+++ spring_ioc_container/src/main/java/org/jsp/animal/Animal_Sound_2.java
@@ -2,21 +2,15 @@ package org.jsp.animal;
 
 public class Animal_Sound_2 {
 	public Animal animal;
-	
-
-
+	private int id;
 
 	public Animal getAnimal() {
 		return animal;
 	}
 
-
-
 	public void setAnimal(Animal animal) {
 		this.animal = animal;
 	}
-
-
 
 	public void sound() {
 		animal.doSound();
