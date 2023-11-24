fb3fcc0875480d4d4fbc1b402afebd6d0a6ca8ce
ManishDevlikar <manish.devlikar5003@gmail.com>
2023-11-24 17:31:27 +0530

    adding scope prototype

--- sring_ioc_annotation/src/main/java/org/jsp/sring_ioc_annotation/collection/Car.java
+++ sring_ioc_annotation/src/main/java/org/jsp/sring_ioc_annotation/collection/Car.java
@@ -1,5 +1,10 @@
 package org.jsp.sring_ioc_annotation.collection;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("prototype") // this will return new object each type by default it was singleton
 public class Car {
 
 }
